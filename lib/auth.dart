2e4071f73c32cc7479537005775602e582573067
nikirana22 <nikirana1983@gmail.com>
2022-11-22 21:13:36 +0530

    refactor(auth):add method for signUp with google

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,8 +1,12 @@
 library auth;
 
+import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Auth {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Auth._();
@@ -21,5 +25,18 @@ class Auth {
     UserCredential userCredential = await _firebaseAuth
         .signInWithEmailAndPassword(email: email, password: password);
     return userCredential.user;
+  }
+
+  void signInWithGoogle() async {
+    GoogleSignInAccount? account = await googleSignIn.signIn();
+    if (account != null) {
+      log('Google signin Account is not null');
+      GoogleSignInAuthentication authentication = await account.authentication;
+      OAuthCredential userCredential = GoogleAuthProvider.credential(
+          accessToken: authentication.accessToken,
+          idToken: authentication.idToken);
+      await _firebaseAuth.signInWithCredential(userCredential);
+      log('user has successfully signup with google');
+    }
   }
 }
